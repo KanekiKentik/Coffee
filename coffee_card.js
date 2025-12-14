1aa6502d715587d027a70b2642ddcc9535dbec93
cripak <ghoulyonish@gmail.com>
2025-12-14 12:33:06 +0300

    compledet functionality, changed layout a lil

--- coffee_card.js
+++ coffee_card.js
@@ -1,11 +1,128 @@
+class Order{
+    constructor(image, name, price, quantity){
+        this.image = image;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
+
 const buttons = {
     hide: document.querySelector("#hide_button"),
     show: document.querySelector("#show_button"),
+
+    order: document.querySelector("#order_button"),
+    rem: document.querySelector("#rem"),
+    add: document.querySelector("#add")
+}
+
+const size_buttons = {
+    short: document.querySelector("#short"),
+    tall: document.querySelector("#tall"),
+    grande: document.querySelector("#grande"),
+    venti: document.querySelector("#venti"),
+    array: [short, tall, grande, venti]
+}
+
+const milk_buttons = {
+    oat: document.querySelector("#oat"),
+    soy: document.querySelector("#soy"),
+    almond: document.querySelector("#almond"),
+    array: [oat, soy, almond]
+}
+
+const extra_buttons = {
+    sugar: document.querySelector("#sugar"),
+    milk: document.querySelector("#milk"),
+    array: [milk, sugar]
+}
+
+let is_extra_chosen = {
+    sugar: false,
+    milk: false,
 }
 
 const elements = {
     screen_shade: document.querySelector(".screen_shade"),
-    slide_panel: document.querySelector(".slide_panel")
+    slide_panel: document.querySelector(".slide_panel"),
+
+    coffee_img: document.querySelector("#coffee_image"),
+    coffee_name: document.querySelector("#coffee_box").querySelector("h1"),
+    coffee_desc: document.querySelector("#coffee_box").querySelector("p"),
+    coffee_price: document.querySelector("#price_and_switch").querySelector("h2"),
+    coffee_quantity: document.querySelector("#switch").querySelector("p"),
+
+    orders: document.querySelector("#order_list"),
+    order_count: document.querySelector("#order_panel").querySelector("h5"),
+
+    price_all: document.querySelector("#price_all")
+}
+
+let cost = {
+    def: 0,
+    size: 0,
+    milk: 0,
+    extra: 0,
+}
+
+const sumCost = () => {
+    return cost.def + cost.size + cost.milk + cost.extra;
+}
+
+let orders = [];
+let quantity = 1;
+const cData = JSON.parse(localStorage.getItem("selectedCoffee"));
+
+function clamp(value, min, max){
+    return Math.min(Math.max(min, value), max);
+}
+
+const UpdateCost = () => {
+    elements.coffee_price.textContent = `$${(sumCost() * quantity).toFixed(2)}`;
+}
+
+const updateQuantity = (change) => {
+    quantity = clamp(quantity + change, 1, 99);
+    elements.coffee_quantity.textContent = quantity;
+
+    UpdateCost();
+}
+
+function PlaceOrder(){
+    order = new Order(cData["orderImage"], cData["name"], sumCost() * quantity, quantity);
+
+    orders.push(order);
+    localStorage.setItem("orders", JSON.stringify(orders));
+}
+
+function ParseOrders(){
+    if(!localStorage.getItem("orders")) return [];
+    jsonOrd = JSON.parse(localStorage.getItem("orders"));
+
+    jsonOrd.forEach(ord => {
+        orders.push(new Order(ord["image"], ord["name"], ord["price"], ord["quantity"]));
+    })
+}
+
+function UpdateOrdersVisual(){
+    elements.orders.innerHTML = ``;
+    let price = 0;
+
+    orders.forEach(ord => {
+        const ordDiv = document.createElement("div");
+        ordDiv.classList.add("order_instance");
+
+        ordDiv.innerHTML = `<img src=${ord.image}>
+                        <h4>${ord.name}</h4>
+                        <p>${ord.quantity}</p>`;
+
+        elements.orders.appendChild(ordDiv);
+
+        price += ord.price;
+    })
+
+    elements.price_all.textContent = `$${price.toFixed(2)}`;
+    elements.order_count.textContent = orders.length;
 }
 
 const InitializeButtons = () => {
@@ -18,11 +135,101 @@ const InitializeButtons = () => {
         elements.screen_shade.classList.toggle("off");
         elements.slide_panel.classList.remove("on");
     })
+
+    buttons.order.addEventListener("click", () => {
+        PlaceOrder();
+        UpdateOrdersVisual();
+        window.location.href = "index.html";
+    })
+
+    buttons.add.addEventListener("click", () => {
+        updateQuantity(1);
+    })
+
+    buttons.rem.addEventListener("click", () => {
+        updateQuantity(-1);
+    })
+
+    size_buttons.array.forEach(but => {
+        but.addEventListener("click", () => PickSize(but, size_buttons.array));
+    })
+
+    milk_buttons.array.forEach(but => {
+        but.addEventListener("click", () => PickMilk(but, milk_buttons.array));
+    })
+
+    extra_buttons.array.forEach(but => {
+        but.addEventListener("click", () => ToggleExtra(but));
+    })
+}
+
+const ButtonSwitch = (button, buttons) => {
+    buttons.forEach(but => {
+        but.classList.remove("picked");
+    });
+
+    button.classList.toggle("picked");
+}
+
+const PickSize = (sizeButton) => {
+    ButtonSwitch(sizeButton, size_buttons.array);
+
+    if(sizeButton == size_buttons.short) cost.size = 0;
+    if(sizeButton == size_buttons.tall) cost.size = 0.5;
+    if(sizeButton == size_buttons.grande) cost.size = 1;
+    if(sizeButton == size_buttons.venti) cost.size = 1.5;
+
+    UpdateCost();
+}
+
+const PickMilk = (milkButton) => {
+    ButtonSwitch(milkButton, milk_buttons.array);
+
+    if(milkButton == milk_buttons.oat) cost.milk = 0;
+    if(milkButton == milk_buttons.soy) cost.milk = 0.3;
+    if(milkButton == milk_buttons.almond) cost.milk = 0.5;
+
+    UpdateCost();
+}
+
+const ToggleExtra = (extraButton) => {
+    extraButton.classList.toggle("picked");
+    switch(extraButton){
+        case extra_buttons.sugar:
+            if(!is_extra_chosen.sugar) cost.extra += 0.1;
+            else cost.extra -= 0.1;
+            is_extra_chosen.sugar = !is_extra_chosen.sugar;
+            break;
+        case extra_buttons.milk:
+            if(!is_extra_chosen.milk) cost.extra += 0.2;
+            else cost.extra -= 0.2;
+            is_extra_chosen.milk = !is_extra_chosen.milk;
+            break;
+    }
+    UpdateCost();
+}
+
+const LoadCurrentCoffee = () => {
+    elements.coffee_img.src = cData["topdownImage"];
+    elements.coffee_name.textContent = cData["name"];
+    elements.coffee_desc.textContent = cData["desc"];
+    cost.def = parseFloat(cData["price"]);
+
+    UpdateCost();
 }
 
 const DocumentInitialization = () => {
     elements.screen_shade.classList.toggle("off");
 
+    PickSize(size_buttons.short);
+    PickMilk(milk_buttons.oat);
+
+    updateQuantity(0);
+    
+    ParseOrders();
+    UpdateOrdersVisual();
+
+    LoadCurrentCoffee();
     InitializeButtons();
 }
 
--- index.js
+++ index.js
@@ -1,11 +1,150 @@
+class CoffeeData{
+    constructor(name, indexImage, topdownImage, orderImage, price, desc){
+        this.indexImage = indexImage;
+        this.topdownImage = topdownImage;
+        this.orderImage = orderImage;
+        this.name = name;
+        this.price = price;
+        this.desc = desc;
+    }
+}
+
+//localStorage.setItem("orders", "");
+let currentFilterButton = NaN;
+
 const buttons = {
     hide: document.querySelector("#hide_button"),
     show: document.querySelector("#show_button"),
 }
 
+const cat_buttons = {
+    cap: document.querySelector("#cap"),
+    ame: document.querySelector("#ame"),
+    lat: document.querySelector("#lat"),
+    array: [lat, ame, cap]
+}
+
 const elements = {
     screen_shade: document.querySelector(".screen_shade"),
-    slide_panel: document.querySelector(".slide_panel")
+    slide_panel: document.querySelector(".slide_panel"),
+
+    coffee_container: document.querySelector("#coffee_box"),
+
+    orders: document.querySelector("#order_list"),
+    order_count: document.querySelector("#order_panel").querySelector("h5"),
+
+    price_all: document.querySelector("#price_all"),
+
+    coffee_search: document.querySelector("#coffee_search"),
+    search_div: document.querySelector(".search_div")
+}
+
+const coffeeList = [
+    new CoffeeData("Cappuccino", "images/Cappuccino/index.jpg", "images/Cappuccino/topdown.png", "images/Cappuccino/order.png", 2.49, "A perfect harmony of bold espresso, steamed milk, and a luxurious layer of velvety foam. Each sip balances rich coffee flavor with creamy sweetness, creating a comforting and classic Italian experience in every cup."),
+    new CoffeeData("Americano", "images/Americano/index.png", "images/Americano/topdown.png", "images/Americano/order.png", 1.99, "Strong, smooth, and elegantly simple. Hot water poured over a shot of espresso creates a clean, robust flavor with a rich aroma. A pure coffee experience for those who appreciate depth without distraction."),
+    new CoffeeData("Latte", "images/Latte/index.jpg", "images/Latte/topdown.png", "images/Latte/order.png", 1.29, "Silky-smooth espresso blended with steamed milk, topped with a light layer of microfoam. Creamy, mild, and endlessly customizable, it's the perfect canvas for latte art and a comforting daily ritual.")
+]
+
+const CreateCoffeeDOM = (cData) => {
+    const cDiv = document.createElement("div");
+    cDiv.innerHTML = `<div class="inner_coffee_box">
+                            <img src="${cData.indexImage}">
+                            <div class="column_between">
+                                <h2>${cData.name}</h2>
+                                <div class="price_tag">
+                                    <p>$${cData.price}</p>
+                                    <button>+</button>
+                                </div>
+                            </div>
+                        </div>`;
+    cDiv.classList.add("coffee_box_instance");
+
+    const transitionButton = cDiv.querySelector("button");
+    transitionButton.addEventListener("click", () => {
+        localStorage.setItem("selectedCoffee", JSON.stringify(cData));
+        window.location.href = "coffee_card.html";
+    })
+
+    return cDiv;
+}
+
+const ButtonSwitch = (button, buttons) => {
+    buttons.forEach(but => {
+        but.classList.remove("picked");
+    });
+
+    if(button == -1) return;
+    button.classList.toggle("picked");
+}
+
+const FillCoffeeBox = (lst, len = 1) => {
+    elements.coffee_container.innerHTML = "";
+    for(i = 0; i < lst.length * len; i++){
+        elements.coffee_container.appendChild(CreateCoffeeDOM(lst[i % lst.length]));
+    }
+}
+
+function UpdateOrdersVisual(orders){
+    elements.orders.innerHTML = ``;
+    let price = 0;
+
+    orders.forEach(ord => {
+        const ordDiv = document.createElement("div");
+        ordDiv.classList.add("order_instance");
+
+        ordDiv.innerHTML = `<img src=${ord.image}>
+                        <h4>${ord.name}</h4>
+                        <p>${ord.quantity}</p>`;
+
+        elements.orders.appendChild(ordDiv);
+
+        price += ord.price;
+    })
+
+    elements.price_all.textContent = `$${price.toFixed(2)}`;
+    elements.order_count.textContent = orders.length;
+}
+
+function GetFilteredCoffeeList(name){
+    newLst = [];
+
+    coffeeList.forEach(cof => {
+        if(cof.name == name) newLst.push(cof);
+    });
+
+    return newLst;
+}
+
+function SetupCategoryButton(but, coffeeType){
+    but.addEventListener("click", () => {
+        if (currentFilterButton != but) {
+            FillCoffeeBox(GetFilteredCoffeeList(coffeeType));
+            currentFilterButton = but;
+            ButtonSwitch(but, cat_buttons.array);
+        }
+        else{
+            FillCoffeeBox(coffeeList, 4);
+            currentFilterButton = NaN;
+            ButtonSwitch(-1, cat_buttons.array);
+        }
+    })
+}
+
+function HandleCoffeeSearch(req){
+    if(req.length == 0){
+        FillCoffeeBox(coffeeList, 4) 
+        return;
+    }
+
+    const coffeeRegex = "^[a-zA-Z]{3,}";
+
+    const match = (req.trim()).match(coffeeRegex);
+
+    if(match){
+        FillCoffeeBox(GetFilteredCoffeeList(match));
+        elements.search_div.classList.remove("red");
+    }
+    else elements.search_div.classList.add("red");
 }
 
 const InitializeButtons = () => {
@@ -18,12 +157,26 @@ const InitializeButtons = () => {
         elements.screen_shade.classList.toggle("off");
         elements.slide_panel.classList.remove("on");
     })
+
+    SetupCategoryButton(cat_buttons.ame, "Americano");
+    SetupCategoryButton(cat_buttons.lat, "Latte");
+    SetupCategoryButton(cat_buttons.cap, "Cappuccino");
 }
 
 const DocumentInitialization = () => {
     elements.screen_shade.classList.toggle("off");
 
     InitializeButtons();
+    FillCoffeeBox(coffeeList, 4);
+
+    orders = JSON.parse(localStorage.getItem("orders"));
+    if(orders){
+        UpdateOrdersVisual(orders);
+    }
+
+    elements.coffee_search.addEventListener("keydown", (ev) => {
+        if(ev.key == "Enter") HandleCoffeeSearch(elements.coffee_search.value);
+    })
 }
 
 DocumentInitialization();
